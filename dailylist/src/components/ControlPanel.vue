790b23c0c6421f27f2bf4e04be3f7a9b88eaf765
Guilherme <guilherme.devon@hotmail.com>
2023-07-09 21:35:44 -0300

    New: Added ControlPanel & TaskList components

---
+++ dailylist/src/components/ControlPanel.vue
@@ -0,0 +1,15 @@
+<template>
+    Olá camarada
+</template>
+
+<script>
+export default {
+  name: 'ControlPanel',
+  props: {
+    isMusicEnabled: Boolean
+  }
+}
+</script>
+
+<style scoped>
+</style>
