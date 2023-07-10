5d86e1fbbea879f082d51f908719cc62196ae6f5
Guilherme <guilherme.devon@hotmail.com>
2023-07-09 21:34:58 -0300

    New: Start of the project

--- dailylist/src/App.vue
+++ dailylist/src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Slab:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Sen&display=swap" rel="stylesheet">
+
+
+  <ControlPanel />
+  <TaskList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ControlPanel from './components/ControlPanel.vue'
+import TaskList from './components/TaskList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ControlPanel, TaskList
   }
 }
 </script>
 
 <style>
+* {
+  background-color: #FCFCF3;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Josefin Slab', serif;
+  font-family: 'Sen', sans-serif;
+
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
