4ff6cc40e5866d89a722f835ae51283fc07a73d7
Guilherme <guilherme.devon@hotmail.com>
2023-07-17 22:13:13 -0300

    Fix: Components gap

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,11 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Slab:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Sen&display=swap" rel="stylesheet">
-    
-    <ControlPanel />
-    
-    <IntroductionText :userName="username" />
-    
-    <TaskList />
+    <div class="components">
+      <ControlPanel />
+      <IntroductionText :userName="username" />
+      <TaskList />
+    </div>
 </template>
 
 <script>
@@ -31,19 +30,29 @@ export default {
 </script>
 
 <style>
-* {
-    background-color: #FCFCF3;
-}
-
 #app {
-    font-family: 'Josefin Slab', serif;
-    /* font-family: 'Sen', sans-serif; */
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
+  font-family: 'Josefin Slab', serif;
+  /* font-family: 'Sen', sans-serif; */
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
 }
 
 h1 {
   font-size: 2.5rem;
+}
+
+hr {
+  width: 80%;
+  margin: 0 auto;
+  border: 1px solid #000;
+}
+
+.components {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1.5vh;
 }
 </style>
