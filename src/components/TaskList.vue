98a74f29ceb288fc85cd183f7b5599c873e3bdc6
Guilherme <guilherme.devon@hotmail.com>
2023-07-17 22:13:56 -0300

    Feat: Start of the list items

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,32 +1,12 @@
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="wrapper">
+   <div class="list-box">
+      <img class="svg-icon mountain-icon" src="../assets/icons/Mountains.svg" alt="Toggle landscape">
+      <div class="inner-shadow"></div>
+      <div>Today's tasks</div>
+      <hr>
+      <div class="control-landscape"></div>
+    </div>
   </div>
 </template>
 
@@ -40,5 +20,36 @@ export default {
 </script>
 
 <style scoped>
+.wrapper {
+  width: 40%;
+  margin: 0 auto;
+  font-size: 1.5rem;
+}
 
+.inner-shadow {
+  width: 100%;
+  height: 100%;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  
+  -moz-box-shadow: inset 0 0 4px #000000;
+  -webkit-box-shadow: inset 0 0 4px #000000;
+  box-shadow: inset 0 0 4px #000000;
+  
+  border-radius: 48px;
+}
+
+.list-box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  min-height: 40vw;
+
+  border: 2px solid #FCFCF3;
+  filter: drop-shadow(0 0 2px #FCFCF3);
+  border-radius: 50%;
+}
 </style>
